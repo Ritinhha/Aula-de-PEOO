b1140c875827c57f4f9a62fe31f8198553c528b6
Rita de <105519177+Ritinhha@users.noreply.github.com>
2022-05-29 19:36:02 +0000

    correção kk

--- Aula do dia 13-05/RevLista02/ex002.cs
+++ Aula do dia 13-05/RevLista02/ex002.cs
@@ -6,27 +6,25 @@ class program{
     int n2 =int.Parse(Console.ReadLine());
     int n3 =int.Parse(Console.ReadLine());
     int n4 =int.Parse(Console.ReadLine());
-int maior;
-if (n1 > n2 && n1 > n3 && n1 > n4){
-  maior = n1;}
-else if (n2 > n1 && n2 > n3 && n2 > n4){
-  maior = n2;}
-else if (n3 > n1 && n3 > n2 && n3 > n4){
-  maior = n3;}
-else {
-  maior = n4;}
-Console.WriteLine($"Maior = {maior}");
-int menor;
-if (n1 < n2 && n1 < n3 && n1 < n4){
-  menor = n1;}
-else if (n2 < n1 && n2 < n3 && n2 < n4){
-  menor = n2;}
-else if (n3 < n1 && n3 < n2 && n3 < n4){
-  menor = n3;}
-else {
-  menor = n4;}
-Console.WriteLine($"Menor = {menor}");
-float media = (n1 + n2 + n3 + n4)/4;
+double media = (n1 + n2 + n3 + n4)/4;
 Console.WriteLine($"Media = {media}");
+Console.WriteLine("Valores maiores ou igual a media");
+if (n1 >= media)
+Console.WriteLine($"{n1}");
+if (n2 >= media)
+Console.WriteLine($"{n2}");
+if (n3 >= media)
+Console.WriteLine($"{n3}");
+if (n4 >= media)
+Console.WriteLine($"{n4}");
+Console.WriteLine("Valores menores que a media");
+if (n1 < media)
+Console.WriteLine($"{n1}");
+if (n2 < media)
+Console.WriteLine($"{n2}");
+if (n3 < media)
+Console.WriteLine($"{n3}");
+if (n4 < media)
+Console.WriteLine($"{n4}");
     }
 }
